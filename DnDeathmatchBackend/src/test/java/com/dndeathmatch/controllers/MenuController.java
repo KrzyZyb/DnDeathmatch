107b49c6bff3a70cdb3113434bd95d545ffad47c
KZYB <krzysztof.zybul@gmail.com>
2020-11-11 19:35:39 +0100

    [Parent] Configuring project build with frontend

---
+++ DnDeathmatchBackend/src/test/java/com/dndeathmatch/controllers/MenuController.java
@@ -0,0 +1,9 @@
+//package com.dndeathmatch.game.controllers;
+//
+//@Controller
+//public class MenuController{
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("forward:/index.html");
+//    }
+//}
